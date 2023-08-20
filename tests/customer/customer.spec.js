bad2db18b43887ff136c9d3ca3b51c14dbde8b3a
Bichntn <37411018+NgocBich2012@users.noreply.github.com>
2023-08-20 21:56:45 +0700

    Update customer.spec.js

--- tests/customer/customer.spec.js
+++ tests/customer/customer.spec.js
@@ -1,8 +1,12 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test.describe("Login Tests", () => {
+test.describe("Customer Tests", () => {
     test('has title', async ({ page }) => {
+        console.log(2)
+    })
+
+    test('has title 1', async ({ page }) => {
         console.log(2)
     })
 });
