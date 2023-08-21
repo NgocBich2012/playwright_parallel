eed3fc4c4d30ccdd86d1b6eaa8c052e7f1c56759
Bichntn <37411018+NgocBich2012@users.noreply.github.com>
2023-08-21 09:38:55 +0700

    Update customer.spec.js

--- tests/customer/customer.spec.js
+++ tests/customer/customer.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require('@playwright/test');
 
 test.describe("Customer Tests", () => {
     test('has title', async ({ page }) => {
-        console.log("customer 1")
+        console.log("customer N")
     })
 
     test('has title 1', async ({ page }) => {
